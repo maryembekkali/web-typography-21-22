5382b59e910c512ef122f888c529c9df119d1a01
maryembekkali <74157536+maryembekkali@users.noreply.github.com>
2022-04-20 13:56:33 +0200

    Update sounds.js

--- closed-captions/sounds.js
+++ closed-captions/sounds.js
@@ -1,15 +1,21 @@
 // Hier kan je tijden toevoegen in secondes
 // Op die mommenten komt er een class op de body.
+// sounds = [
+// 	0,
+// 	9,
+// 	12,
+// 	15,
+// 	16.5,
+// 	24,
+// 	25.8,
+// 	31,
+// 	32,
+// 	34,
+// 	86
+// ];
+
+
 sounds = [
 	0,
-	9,
-	12,
-	15,
-	16.5,
-	24,
-	25.8,
-	31,
-	32,
-	34,
-	86
+	20
 ];
